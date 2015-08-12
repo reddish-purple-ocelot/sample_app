9f42dbd4d8d731f863ef31d9aea0ebe81ddd8a33
reddish-purple-ocelot <val.slyun@gmail.com>
2015-08-12 17:42:35 +0300

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 SampleApp::Application.routes.draw do
+  get "users/new"
   root 'static_pages#home'
   # get 'home' => 'static_pages#home'
   get 'help' => 'static_pages#help'
   get 'about' => 'static_pages#about' 
   get 'contact' => 'static_pages#contact'
+  get 'signup' => 'users#new'
 
 
 
