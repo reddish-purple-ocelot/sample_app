c472b29f389eb64106924b9d776e26ea3513e8b2
reddish-purple-ocelot <val.slyun@gmail.com>
2015-08-17 18:03:32 +0300

    Change user model. Add password_digest

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,12 +2,15 @@ require 'spec_helper'
 
 describe User do
 
-  before { @user =  User.new(name: "Val", email: "val.slyun@gmail.com") }
+  before { @user =  User.new(name: "Val", email: "val.slyun@gmail.com", password: "foobar", password_comfirmation: "foobar") }
 
   subject { @user }
 
   it { should respond_to(:name) }
   it { should respond_to(:email) }
+  it { should respond_to(:password_digest) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
 
   it { should be_valid }
 
@@ -55,6 +58,11 @@ describe User do
       user_with_same_email.save
     end
 
+    it { should_not be_valid }
+  end
+
+  describe "when password is not present" do
+    before { @user.password = @user.password_confirmation = " " }
     it { should_not be_valid }
   end
 end
