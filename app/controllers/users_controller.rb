7700c5bf9f4af8cd17a1e161cce136fa39437ce6
reddish-purple-ocelot <val.slyun@gmail.com>
2015-09-04 16:36:13 +0300

    Displays error messages when you login.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_param)    # Not the final implementation!
+    @user = User.new(user_params)    # Not the final implementation!
     if @user.save
       # Handle a successful save.
     else
