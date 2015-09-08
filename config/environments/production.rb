3d023549bc1da03f6078353dddbf8546c7191731
reddish-purple-ocelot <val.slyun@gmail.com>
2015-09-08 18:45:47 +0300

    del dump_schema_after_migration in production file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,5 +76,5 @@ SampleApp::Application.configure do
 
   # Do not dump schema after migrations.
   
-  config.active_record.dump_schema_after_migration = true
+  # config.active_record.dump_schema_after_migration = false
 end
