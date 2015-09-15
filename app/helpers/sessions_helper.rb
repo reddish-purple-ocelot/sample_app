9bc151167be31724ee917a83b2ec3286c9d0f0ec
reddish-purple-ocelot <val.slyun@gmail.com>
2015-09-15 17:40:06 +0300

    Added links for header

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,8 +3,12 @@ module SessionsHelper
   def sign_in(user)
     remember_token = User.new_remember_token
     cookies.permanent[:remember_token] = remember_token
-    user.update_attribute(:remember_token, User.encrypte(remember_token))
+    user.update_attribute(:remember_token, User.encrypt(remember_token))
     self.current_user = user
+  end
+
+  def signed_in?
+    !current_user.nil?
   end
 
   def current_user=(user)
@@ -12,7 +16,7 @@ module SessionsHelper
   end
 
   def current_user
-    remember_token = User.encrypte(cookies[:remember_token])
+    remember_token = User.encrypt(cookies[:remember_token])
     @current_user ||= User.find_by(remember_token: remember_token)
   end
 end
