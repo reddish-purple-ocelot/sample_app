7dac9cc7ee1a9c220c95e2ba59d5b4464fb9e6cb
reddish-purple-ocelot <val.slyun@gmail.com>
2015-09-24 18:08:25 +0300

    Added new tests on edit and update data others users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :signed_in_user, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :updated]
   def show
     @user = User.find(params[:id])    
   end
@@ -20,7 +21,7 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
+    # @user = User.find(params[:id])
   end
 
   def update
@@ -43,5 +44,10 @@ class UsersController < ApplicationController
 
     def signed_in_user
       redirect_to signin_url, notice: "Please sign in." unless signed_in?
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to (root_url) unless current_user?(@user)
     end
 end
