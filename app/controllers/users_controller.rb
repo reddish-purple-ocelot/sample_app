8494a77f83e981a55e0138d1f214e7e69148b408
reddish-purple-ocelot <val.slyun@gmail.com>
2015-09-28 11:37:11 +0300

    Added test for user index non-autorization action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :signed_in_user, only: [:edit, :update]
+  before_action :signed_in_user, only: [:index, :edit, :update]
   before_action :correct_user, only: [:edit, :update]
   def show
     @user = User.find(params[:id])    
@@ -20,12 +20,14 @@ class UsersController < ApplicationController
     end
   end
 
+  def index
+  end
+
   def edit
-    # @user = User.find(params[:id])
+    
   end
 
   def update
-    # @user = User.find(params[:id])
     if @user.update_attributes(user_params)
       flash[:success] = "Profile updated"
       redirect_to @user      
